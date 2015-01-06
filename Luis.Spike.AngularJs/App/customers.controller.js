14818206c5d2c0f4f5e4775a787ebbe1887c18c9
Luis Blanco 路易斯.布兰科 <luis_blanco@volusion.com>
2015-01-06 12:51:32 -0600

    First commit

---
+++ Luis.Spike.AngularJs/App/customers.controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('registrationModule')
+        .controller('customersController', customersController);
+
+    customersController.$inject = ['$location'];
+
+    function customersController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'customersController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ Luis.Spike.AngularJs/App/registration-module.js
@@ -0,0 +1,24 @@
+﻿
+    'use strict';
+
+    var registrationModule = angular
+        .module('registrationModule', []
+        //.config(function($routeProvider, $locationProvider) {
+        //    console.log($routeProvider);
+        //    $locationProvider.html5Mode(true);
+        //}
+        );
+
+    //var registrationModule = angular.module('app', [
+    //    // Angular modules 
+    //     'ngRoute',
+    //     'ngResource',
+    //    // Custom modules 
+
+    //    // 3rd Party Modules
+    //    'restangular',
+    //    'ngTable'
+    //]).config(function ($routeProvider, $locationProvider) {
+    //    console.log($routeProvider);
+    //    $locationProvider.html5Mode(true);
+//});/['ngRoute', 'ngResource']
